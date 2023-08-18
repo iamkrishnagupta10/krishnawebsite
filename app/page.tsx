9b9393fed203a436031a4894bf385ff39dce4b9d
Krishna Gupta <129900110+iamkrishnagupta10@users.noreply.github.com>
2023-08-17 17:34:40 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,17 +29,17 @@ export default function Home() {
 				quantity={100}
 			/>
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				chronark
+				misty
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi, my name is Andreas, I'm building serverless and open source
+					Hi, my name is Krishna, I'm building smart websites and apps using ai and open source
 					solutions at{" "}
 					<Link
 						target="_blank"
-						href="https://upstash.com"
+						href="https://mistyinteractivestudios.com"
 						className="underline duration-500 hover:text-zinc-300"
 					>
 						Upstash
